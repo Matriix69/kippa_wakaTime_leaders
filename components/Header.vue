5507c6f7a29000b56f18f8e6cb1dac6c641cab0a
Isaac Chukwuka <matrix@Isaacs-MacBook.local>
2023-01-15 19:44:13 +0100

    correct header

--- components/Header.vue
+++ components/Header.vue
@@ -10,12 +10,12 @@ export default {
 </script>
 
 <template>
-    <div class="flex flex-col lg:flex-row my-8">
+    <div class="flex flex-col sm:flex-row my-8">
         <h1 class="font-bold text-2xl items-center">WakaTime Leaders</h1>
         <input
             v-on:input="searchFunction"
             placeholder="search"
-            class="p-4 rounded-md-auto w-full max-w-[210px] mt-5 lg:ml-auto lg:mt-0"
+            class="p-4 rounded-md-auto w-full max-w-100% mt-5 sm:max-w-[210px] sm:ml-auto lg:mt-0"
         />
     </div>
 </template>
